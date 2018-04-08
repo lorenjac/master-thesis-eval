04137c6e9a46815959e6671123a4bc4883e7eb9c
Jacob Lorenz <jacob.lorenz@b-tu.de>
2018-04-08 23:40:33 +0200

    Figures are now exported to pdf format

--- make-fig-spd.sh
+++ make-fig-spd.sh
@@ -1,4 +1,4 @@
-python plot-speedup.py -o fig/spd-ss.png stats/spd/midas-spd-ss.csv stats/spd/midas-unlocked-spd-ss.csv stats/spd/echo-spd-ss.csv
-python plot-speedup.py -o fig/spd-sl.png stats/spd/midas-spd-sl.csv stats/spd/midas-unlocked-spd-sl.csv stats/spd/echo-spd-sl.csv
-python plot-speedup.py -o fig/spd-ls.png stats/spd/midas-spd-ls.csv stats/spd/midas-unlocked-spd-ls.csv stats/spd/echo-spd-ls.csv
-python plot-speedup.py -o fig/spd-ll.png stats/spd/midas-spd-ll.csv stats/spd/midas-unlocked-spd-ll.csv stats/spd/echo-spd-ll.csv
+python plot-speedup.py -o fig/spd-ss.pdf stats/spd/midas-spd-ss.csv stats/spd/midas-unlocked-spd-ss.csv stats/spd/echo-spd-ss.csv
+python plot-speedup.py -o fig/spd-sl.pdf stats/spd/midas-spd-sl.csv stats/spd/midas-unlocked-spd-sl.csv stats/spd/echo-spd-sl.csv
+python plot-speedup.py -o fig/spd-ls.pdf stats/spd/midas-spd-ls.csv stats/spd/midas-unlocked-spd-ls.csv stats/spd/echo-spd-ls.csv
+python plot-speedup.py -o fig/spd-ll.pdf stats/spd/midas-spd-ll.csv stats/spd/midas-unlocked-spd-ll.csv stats/spd/echo-spd-ll.csv
